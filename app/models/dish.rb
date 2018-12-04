556c92bec89bd59bfd46375d8ca27d82815f8e86
Carlos Torres <andyandroid412@gmail.com>
2018-12-03 21:27:09 -0800

    Ya se pueden subir imagenes, el formulario de nuevo producto ya tiene un mejor estilo, faltan validaciones en el modelo de rails llamado dish, y mejorar la relacion de aspecto de las imagenes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,15 @@
 class Dish < ApplicationRecord
+	before_destroy :not_referenced_by_any_line_item
+  belongs_to :user, optional: true
+  has_many :line_items
+
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON # If you use SQLite, add this line
+
+  def not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
